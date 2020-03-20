3c5700d8a43ab40b4ae9bdf6f608d4d6166b954f
Stephanie Sohns <ssohns@gmail.com>
2020-03-20 10:18:48 -0600

    Iteration1part

---
+++ src/part.js
@@ -0,0 +1,35 @@
+
+  class Part {
+  constructor(object) {
+    this.name = object.name;
+    this.type =
+    object.type === 'shell' ||
+    object.type === 'hyperdrive' ||
+    object.type === 'computer' ||
+    object.type === 'life support' ||
+    object.type === 'landing gear' ?
+    object.type : undefined;
+    this.value = object.value;
+    this.broken = false;
+  }
+
+  isValid() {
+    if (this.name === undefined){
+      return false;
+    }
+    if (this.type === undefined) {
+      return false;
+    }
+    if (this.value === undefined) {
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+
+}
+
+
+
+module.exports = Part;
